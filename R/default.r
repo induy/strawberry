21238e29da5be92defdbb6f0e5fe5c9d0e4923e3
induy <sinduy@nate.com>
2019-07-15 16:58:51 +0900

    update

--- R/default.r
+++ R/default.r
@@ -12,3 +12,12 @@
 #'
 #' data(StrawBerry)
 "StrawBerry"
+berry_function <- function(seolhyang=TRUE){
+  if(seolhyang==TRUE){
+    print("I love seolhyang strawberry!")
+  }
+  else {
+    print("I am not a human.")
+  }
+}
+
